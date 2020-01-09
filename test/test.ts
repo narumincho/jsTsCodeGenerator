fe9e7d7d8e02f527c200a9c49e092adbf734b103
narumincho <narumincho.starfy@gmail.com>
2020-01-09 11:37:17 +0900

    add test "performance"

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,14 @@
 import * as main from "../source/main";
+import { performance } from "perf_hooks";
 
 test("return string", () => {
   expect(typeof main.toJavaScriptCode()).toBe("string");
+});
+
+test("performance", () => {
+  const start = performance.now();
+  main.toJavaScriptCode();
+  const time = performance.now() - start;
+  console.log(time.toString() + "ms");
+  expect(time).toBeLessThan(10000);
 });
